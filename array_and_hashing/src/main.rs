188a479bbde0cca0afbfeb8c3e61628d08eb8ea9
bentenismyidol <nguyenvietthanh18112002@gmail.com>
2024-11-02 21:50:43 +0700

    Had completed 3 easy + 1 med so far

--- array_and_hashing/src/main.rs
+++ array_and_hashing/src/main.rs
@@ -1,8 +1,20 @@
-use std::collections::{BTreeMap, HashMap};
+use std::{
+    collections::{BTreeMap, HashMap},
+    iter,
+    ops::Deref,
+    vec,
+};
 
 fn main() {
-    let vec = vec![2, 7, 11, 15];
-    two_sum(vec, 9);
+    let vec: Vec<String> = vec![
+        "eat".to_string(),
+        "tea".to_string(),
+        "tan".to_string(),
+        "ate".to_string(),
+        "nat".to_string(),
+        "bat".to_string(),
+    ];
+    group_anagrams(vec);
 }
 
 // Problem 210: Contain duplication - Easy
@@ -45,4 +57,27 @@ fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
         };
     }
     vec![]
+}
+
+//problem 49: Group Anagrams - Medium
+fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
+    let mut result_map: HashMap<String, Vec<String>> = HashMap::new();
+    for elements in strs.into_iter() {
+        let mut sorted_elements_as_key = sort_string_ref(elements.deref());
+        println!("elements after sort as key: {}", sorted_elements_as_key);
+        result_map
+            .entry(sorted_elements_as_key)
+            .or_insert(Vec::new())
+            .push(elements);
+    }
+    println!("{:?}", result_map);
+    let result = result_map.into_values().collect();
+    println!("{:?}", result);
+    result
+}
+fn sort_string_ref(input: &str) -> String {
+    let mut char_vec: Vec<char> = input.chars().collect();
+    char_vec.sort_unstable();
+    let output: String = char_vec.into_iter().collect();
+    output
 }
