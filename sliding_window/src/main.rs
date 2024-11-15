751a618bd4615fa98130ec3c6fd9763a90eee0d2
bentenismyidol <nguyenvietthanh18112002@gmail.com>
2024-11-15 20:36:38 +0700

    1 Medium

--- sliding_window/src/main.rs
+++ sliding_window/src/main.rs
@@ -1,8 +1,8 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 fn main() {
-    let prices = vec![7, 1, 6, 5, 4, 2];
-    max_profit(prices);
+    let s = "qrsvbspk".to_string();
+    length_of_longest_substring(s);
 }
 
 //problem 121: Best time to buy and Sell stock - Easy
@@ -19,4 +19,25 @@ fn max_profit(prices: Vec<i32>) -> i32 {
     } else {
         esstimate_profit
     };
+}
+
+//problem 3. Longest Substring without Repeating Characters
+fn length_of_longest_substring(s: String) -> i32 {
+    let mut set = HashSet::new();
+    let mut vec: Vec<char> = s.chars().collect();
+    let mut left = 0;
+    let mut res = 0;
+    for right in 0..vec.len() {
+        while (set.contains(&vec[right])) {
+            println!("pre set {set:?}");
+            set.remove(&vec[left]);
+            left += 1;
+            println!("post set {set:?}");
+        }
+        set.insert(&vec[right]);
+        res = res.max(set.len());
+        println!("set {set:?}")
+    }
+    println!("res {res}");
+    res as i32
 }
