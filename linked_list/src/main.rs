c4d1459fda6ab7400fdd95c87ff7d2bb74d1dcad
bentenismyidol <nguyenvietthanh18112002@gmail.com>
2025-02-04 21:16:48 +0700

    1 Easy

--- linked_list/src/main.rs
+++ linked_list/src/main.rs
@@ -1,18 +1,38 @@
 fn main() {
-    let head = Some(Box::new(ListNode {
+    let head1 = Some(Box::new(ListNode {
         val: 1,
         next: Some(Box::new(ListNode {
             val: 2,
             next: Some(Box::new(ListNode {
-                val: 3,
+                val: 4,
                 next: Some(Box::new(ListNode {
-                    val: 4,
-                    next: Some(Box::new(ListNode { val: 5, next: None })),
+                    val: 8,
+                    next: Some(Box::new(ListNode {
+                        val: 12,
+                        next: None,
+                    })),
                 })),
             })),
         })),
     }));
-    reverse_list(head);
+    let head2 = Some(Box::new(ListNode {
+        val: 1,
+        next: Some(Box::new(ListNode {
+            val: 3,
+            next: Some(Box::new(ListNode {
+                val: 4,
+                next: Some(Box::new(ListNode {
+                    val: 9,
+                    next: Some(Box::new(ListNode {
+                        val: 11,
+                        next: None,
+                    })),
+                })),
+            })),
+        })),
+    }));
+
+    merge_two_lists(head1, head2);
 }
 #[derive(PartialEq, Eq, Clone, Debug)]
 pub struct ListNode {
@@ -27,18 +47,48 @@ impl ListNode {
     }
 }
 
-//206. Reverse Linked List
+//206. Reverse Linked List - Easy
 fn reverse_list(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
     let mut pointer = None;
     let mut current_pointer = head;
     while let Some(mut node) = current_pointer {
+        println!("the value is {}", node.val);
         let next = node.next.take();
         node.next = pointer.take();
         pointer = Some(node);
         current_pointer = next;
     }
+    //DEBUG
     if let Some(result) = &pointer {
         println!("result: {}", result.val)
     }
     pointer
+}
+
+//21. Merge two sorted lists - Easy
+fn merge_two_lists(
+    list1: Option<Box<ListNode>>,
+    list2: Option<Box<ListNode>>,
+) -> Option<Box<ListNode>> {
+    let mut prehead = ListNode::new(-1);
+    let mut curr = &mut prehead;
+
+    let mut head1 = list1;
+    let mut head2 = list2;
+
+    while let (Some(node1), Some(node2)) = (&head1, &head2) {
+        println!("the node1 value is {}", node1.val);
+        println!("the node2 value is {}", node2.val);
+        if (node1.val <= node2.val) {
+            curr.next = head1.take();
+            curr = curr.next.as_mut().unwrap();
+            head1 = curr.next.take();
+        } else {
+            curr.next = head2.take();
+            curr = curr.next.as_mut().unwrap();
+            head2 = curr.next.take();
+        }
+    }
+    curr.next = head1.or(head2);
+    prehead.next
 }
---
+++ linked_list/target/debug/deps/linked_list-deb2c00d95943a7a.d
@@ -0,0 +1,5 @@
+D:\RustProjects\LeetcodeArchive\linked_list\target\debug\deps\liblinked_list-deb2c00d95943a7a.rmeta: src\main.rs
+
+D:\RustProjects\LeetcodeArchive\linked_list\target\debug\deps\linked_list-deb2c00d95943a7a.d: src\main.rs
+
+src\main.rs:
