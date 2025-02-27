8dfd0525ceb42354b4356efb6aaf639193d3d866
thanhnguyenGit <nguyenvietthanh18112002@gmail.com>
2025-02-27 23:02:41 +0700

    1 Med

--- linked_list/src/main.rs
+++ linked_list/src/main.rs
@@ -1,3 +1,5 @@
+use std::mem;
+
 fn main() {
     let mut head1 = Some(Box::new(ListNode {
         val: 1,
@@ -97,32 +99,50 @@ fn merge_two_lists(
 }
 
 //143. Reorder List
-fn reorder_list(head : &mut Option<Box<ListNode>>) {
+fn reorder_list(head: &mut Option<Box<ListNode>>) {
     if head.is_none() || head.as_ref().unwrap().next.is_none() {
         return;
     }
     // Traverse and printf the linked list
     let mut pointer = head.as_ref();
     while let Some(node) = pointer {
-        println!("Node val: {}", node.val);
-        pointer  = node.next.as_ref();
+        pointer = node.next.as_ref();
     }
 
     // find the middle of the list
-    let mut slow = head.as_ref();
-    let mut fast = head.as_ref();
+    let mut slow = head.clone();
+    let mut fast = head.clone();
+    let mut slow_clone = None;
     let mut mid = 0;
-    while let (Some(slow_node), Some(fast_node)) = (slow,fast.as_ref().unwrap().next.as_ref()) {
-        println!("Slow: {}, Fast: {}", slow_node.val, fast_node.val);
-        slow = slow_node.next.as_ref();
-        fast = fast_node.next.as_ref();
+    while let (Some(slow_node), Some(fast_node)) = (slow, fast.unwrap().next.as_ref()) {
+        slow = slow_node.next;
+        fast = fast_node.next.clone();
 
         if fast.is_none() {
             mid = slow.as_ref().unwrap().val;
-            println!("Middle {}", mid);
-            break
+            slow_clone = slow.clone();
+            break;
         }
     }
-    let second = &slow.unwrap().next;
-    println!("second {}", second.as_ref().unwrap().val);
+    // reverse the other half start from the mid
+    let mut second_half = slow_clone.unwrap().next;
+    // use prob 21 reverse linked list.
+    let mut reversed = reverse_list(second_half);
+    let mut first_half = head;
+    merge_two_lists_zig_zag(first_half, &mut reversed);
 }
+
+pub fn merge_two_lists_zig_zag(
+    mut list1: &mut Option<Box<ListNode>>,
+    mut list2: &mut Option<Box<ListNode>>,
+) -> () {
+    if list1.is_none() {
+        mem::swap(list1, list2);
+        return;
+    }
+    if list2.is_some() {
+        list1 = &mut list1.as_mut().unwrap().next;
+        mem::swap(list1, list2);
+    }
+}
+
